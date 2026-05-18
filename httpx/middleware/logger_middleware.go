854988fdaf086a0a9fafa8c4bded3465952a38cf
Bruno Luiz Silva <contact@brunoluiz.net>
2026-05-18 21:37:01 +0100

    fix: logger

--- httpx/middleware/logger_middleware.go
+++ httpx/middleware/logger_middleware.go
@@ -29,7 +29,7 @@ func (rec *statusRecorder) Write(data []byte) (int, error) {
 	return n, err
 }
 
-func LoggerMiddleware(logger interface {
+func Logger(logger interface {
 	Info(string, ...any)
 },
 ) Middleware {
